dcba90c2d81fee993d65caad323ed5a66ffd0aa0
nataliaangelina20 <103477003+nataliaangelina20@users.noreply.github.com>
2022-04-13 09:52:47 +0800

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const HOST = '0.0.0.0'
 const app = express()
 app.get('/', (req, res) => {
     res.send('Natalia Angelina\n')
+    res.send('52019020\n')
+    res.send('Teknik Informatika\n')
 })
 
 app.listen(PORT, HOST)
